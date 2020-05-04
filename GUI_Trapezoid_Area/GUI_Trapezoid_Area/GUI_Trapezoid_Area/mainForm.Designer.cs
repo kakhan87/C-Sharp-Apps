1f1aafcab914a24aa6c46b7cc71e5b6c9023013e
Karim Khan <kakhan87@gmail.com>
2020-05-04 13:50:49 -0600

    calculates Trapezoid area using GUI App

---
+++ GUI_Trapezoid_Area/GUI_Trapezoid_Area/GUI_Trapezoid_Area/mainForm.Designer.cs
@@ -0,0 +1,156 @@
+﻿namespace GUI_Trapezoid_Area
+{
+    partial class mainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.firstParallelSideTextBox = new System.Windows.Forms.TextBox();
+            this.firstParallelSideLabel = new System.Windows.Forms.Label();
+            this.secondParallelSideTextBox = new System.Windows.Forms.TextBox();
+            this.secondParallelSideLabel = new System.Windows.Forms.Label();
+            this.heightTextBox = new System.Windows.Forms.TextBox();
+            this.heightLabel = new System.Windows.Forms.Label();
+            this.calculateButton = new System.Windows.Forms.Button();
+            this.quitButton = new System.Windows.Forms.Button();
+            this.resultOutputLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // firstParallelSideTextBox
+            // 
+            this.firstParallelSideTextBox.Location = new System.Drawing.Point(144, 20);
+            this.firstParallelSideTextBox.Name = "firstParallelSideTextBox";
+            this.firstParallelSideTextBox.Size = new System.Drawing.Size(100, 20);
+            this.firstParallelSideTextBox.TabIndex = 1;
+            // 
+            // firstParallelSideLabel
+            // 
+            this.firstParallelSideLabel.AutoSize = true;
+            this.firstParallelSideLabel.Location = new System.Drawing.Point(13, 23);
+            this.firstParallelSideLabel.Name = "firstParallelSideLabel";
+            this.firstParallelSideLabel.Size = new System.Drawing.Size(125, 13);
+            this.firstParallelSideLabel.TabIndex = 1;
+            this.firstParallelSideLabel.Text = "Length of Parallel Side 1:";
+            // 
+            // secondParallelSideTextBox
+            // 
+            this.secondParallelSideTextBox.Location = new System.Drawing.Point(144, 48);
+            this.secondParallelSideTextBox.Name = "secondParallelSideTextBox";
+            this.secondParallelSideTextBox.Size = new System.Drawing.Size(100, 20);
+            this.secondParallelSideTextBox.TabIndex = 2;
+            // 
+            // secondParallelSideLabel
+            // 
+            this.secondParallelSideLabel.AutoSize = true;
+            this.secondParallelSideLabel.Location = new System.Drawing.Point(13, 51);
+            this.secondParallelSideLabel.Name = "secondParallelSideLabel";
+            this.secondParallelSideLabel.Size = new System.Drawing.Size(125, 13);
+            this.secondParallelSideLabel.TabIndex = 1;
+            this.secondParallelSideLabel.Text = "Length of Parallel Side 2:";
+            // 
+            // heightTextBox
+            // 
+            this.heightTextBox.Location = new System.Drawing.Point(144, 77);
+            this.heightTextBox.Name = "heightTextBox";
+            this.heightTextBox.Size = new System.Drawing.Size(100, 20);
+            this.heightTextBox.TabIndex = 3;
+            // 
+            // heightLabel
+            // 
+            this.heightLabel.AutoSize = true;
+            this.heightLabel.Location = new System.Drawing.Point(49, 80);
+            this.heightLabel.Name = "heightLabel";
+            this.heightLabel.Size = new System.Drawing.Size(89, 13);
+            this.heightLabel.TabIndex = 1;
+            this.heightLabel.Text = "Length of Height:";
+            // 
+            // calculateButton
+            // 
+            this.calculateButton.Location = new System.Drawing.Point(18, 110);
+            this.calculateButton.Name = "calculateButton";
+            this.calculateButton.Size = new System.Drawing.Size(75, 23);
+            this.calculateButton.TabIndex = 4;
+            this.calculateButton.Text = "Calculate";
+            this.calculateButton.UseVisualStyleBackColor = true;
+            this.calculateButton.Click += new System.EventHandler(this.calculateButton_Click);
+            // 
+            // quitButton
+            // 
+            this.quitButton.Location = new System.Drawing.Point(18, 139);
+            this.quitButton.Name = "quitButton";
+            this.quitButton.Size = new System.Drawing.Size(75, 23);
+            this.quitButton.TabIndex = 0;
+            this.quitButton.TabStop = false;
+            this.quitButton.Text = "Quit";
+            this.quitButton.UseVisualStyleBackColor = true;
+            this.quitButton.Click += new System.EventHandler(this.quitButton_Click);
+            // 
+            // resultOutputLabel
+            // 
+            this.resultOutputLabel.ForeColor = System.Drawing.Color.Blue;
+            this.resultOutputLabel.Location = new System.Drawing.Point(105, 109);
+            this.resultOutputLabel.Name = "resultOutputLabel";
+            this.resultOutputLabel.Size = new System.Drawing.Size(100, 23);
+            this.resultOutputLabel.TabIndex = 3;
+            this.resultOutputLabel.Text = "Area Result";
+            this.resultOutputLabel.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.resultOutputLabel.Visible = false;
+            // 
+            // mainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(278, 175);
+            this.Controls.Add(this.resultOutputLabel);
+            this.Controls.Add(this.quitButton);
+            this.Controls.Add(this.calculateButton);
+            this.Controls.Add(this.heightLabel);
+            this.Controls.Add(this.heightTextBox);
+            this.Controls.Add(this.secondParallelSideLabel);
+            this.Controls.Add(this.secondParallelSideTextBox);
+            this.Controls.Add(this.firstParallelSideLabel);
+            this.Controls.Add(this.firstParallelSideTextBox);
+            this.Name = "mainForm";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox firstParallelSideTextBox;
+        private System.Windows.Forms.Label firstParallelSideLabel;
+        private System.Windows.Forms.TextBox secondParallelSideTextBox;
+        private System.Windows.Forms.Label secondParallelSideLabel;
+        private System.Windows.Forms.TextBox heightTextBox;
+        private System.Windows.Forms.Label heightLabel;
+        private System.Windows.Forms.Button calculateButton;
+        private System.Windows.Forms.Button quitButton;
+        private System.Windows.Forms.Label resultOutputLabel;
+    }
+}
+
---
+++ GUI_Trapezoid_Area/GUI_Trapezoid_Area/GUI_Trapezoid_Area/mainForm.cs
@@ -0,0 +1,96 @@
+﻿// Program Name:    T04 P01 GUI Trapezoid Area
+// Author:          Karim Khan
+//
+// Date:            06-April-2020
+//
+// Description:     Mini-Project Assignment ‐ Graphical User Interfaces
+//
+//                  Implement an application that allows the user to calculate the area of a Trapezoid. 
+//                  The interface will have:
+//                  1. A textbox for entering the length of one of the parallel sides
+//                  2. Another textbox for entering the length of the other parallel side
+//                  3. A third textbox that for entering the height; the distance between the two parallel sides
+//                  4. A Calculate button that when pressed, will first validate the numbers entered into the text 
+//                  boxes.If they are invalid lengths(not numeric or have values less than or equal to zero) an appropriate, 
+//                  specific error message is to be displayed in an alert box(message box). If however the value are valid 
+//                  then the area of the trapezoid is to be displayed in an area beside the calculate button.
+//                  5. A Quit button which when pressed will cause the application to exit.
+//
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GUI_Trapezoid_Area
+{
+    public partial class mainForm : Form
+    {
+        public mainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void quitButton_Click(object sender, EventArgs e)
+        {
+            Application.Exit(); // exits the Application
+        }
+
+        private void calculateButton_Click(object sender, EventArgs e)
+        {
+            double firstParallelSide;
+            double secondParallelSide;
+            double height;
+
+            // get the trapezoid's first parallel side
+            if (!double.TryParse(firstParallelSideTextBox.Text, out firstParallelSide))
+            {
+                MessageBox.Show("First Parallel Side is not a number.");
+                return; // return will exit the function when calling it - if no valid base length is provided, 
+                        // it ends the method "void" and nothing below the return statement will be executed.
+            }
+
+            if (firstParallelSide <= 0)
+            {
+                MessageBox.Show("First Parallel Side must be a positive number.");
+                return;
+            }
+            // get the trapezoid's second parallel side
+            if (!double.TryParse(secondParallelSideTextBox.Text, out secondParallelSide))
+            {
+                MessageBox.Show("Second Parallel Side is not a number.");
+                return; // return will exit the function when calling it - if no valid base length is provided, 
+                        // it ends the method "void" and nothing below the return statement will be executed.
+            }
+
+            if (secondParallelSide <= 0)
+            {
+                MessageBox.Show("Second Parallel Side must be a positive number.");
+                return;
+            }
+            // get the trapezoid height
+
+            if (!double.TryParse(heightTextBox.Text, out height))
+            {
+                MessageBox.Show("Triangle Height is not a number.");
+                return;
+            }
+
+            if (height <= 0)
+            {
+                MessageBox.Show("Trapezoid Height must be a positive number.");
+                return;
+            }
+            // do the calculation and update the results label
+
+            double area = ((firstParallelSide + secondParallelSide) / 2) * height; // inline declaration of area
+
+            resultOutputLabel.Text = $"Area = {area:N2}";
+            resultOutputLabel.Visible = true;
+        }
+    }
+}
