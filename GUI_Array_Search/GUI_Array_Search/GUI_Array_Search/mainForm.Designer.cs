911568080ce625ed805cfbb6369dba4e350baed1
Karim Khan <kakhan87@gmail.com>
2020-05-04 13:46:51 -0600

    Array Search GUI App

---
+++ GUI_Array_Search/GUI_Array_Search/GUI_Array_Search/mainForm.Designer.cs
@@ -0,0 +1,120 @@
+﻿namespace GUI_Array_Search
+{
+    partial class mainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.searchTextBox = new System.Windows.Forms.TextBox();
+            this.searchButton = new System.Windows.Forms.Button();
+            this.searchOutputLabel = new System.Windows.Forms.Label();
+            this.randomOutputTextBox = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(31, 27);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(109, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Random Number List:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(36, 57);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(104, 13);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Value to Search For:";
+            // 
+            // searchTextBox
+            // 
+            this.searchTextBox.Location = new System.Drawing.Point(143, 55);
+            this.searchTextBox.Name = "searchTextBox";
+            this.searchTextBox.Size = new System.Drawing.Size(53, 20);
+            this.searchTextBox.TabIndex = 1;
+            // 
+            // searchButton
+            // 
+            this.searchButton.Location = new System.Drawing.Point(208, 54);
+            this.searchButton.Name = "searchButton";
+            this.searchButton.Size = new System.Drawing.Size(75, 23);
+            this.searchButton.TabIndex = 2;
+            this.searchButton.Text = "Search";
+            this.searchButton.UseVisualStyleBackColor = true;
+            this.searchButton.Click += new System.EventHandler(this.searchButton_Click);
+            // 
+            // searchOutputLabel
+            // 
+            this.searchOutputLabel.Location = new System.Drawing.Point(291, 55);
+            this.searchOutputLabel.Name = "searchOutputLabel";
+            this.searchOutputLabel.Size = new System.Drawing.Size(255, 24);
+            this.searchOutputLabel.TabIndex = 0;
+            this.searchOutputLabel.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.searchOutputLabel.Visible = false;
+            // 
+            // randomOutputTextBox
+            // 
+            this.randomOutputTextBox.Location = new System.Drawing.Point(143, 26);
+            this.randomOutputTextBox.Name = "randomOutputTextBox";
+            this.randomOutputTextBox.ReadOnly = true;
+            this.randomOutputTextBox.Size = new System.Drawing.Size(328, 20);
+            this.randomOutputTextBox.TabIndex = 3;
+            // 
+            // mainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(610, 111);
+            this.Controls.Add(this.randomOutputTextBox);
+            this.Controls.Add(this.searchButton);
+            this.Controls.Add(this.searchTextBox);
+            this.Controls.Add(this.searchOutputLabel);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "mainForm";
+            this.Text = "Searching an Array GUI Application";
+            this.Load += new System.EventHandler(this.mainForm_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox searchTextBox;
+        private System.Windows.Forms.Button searchButton;
+        private System.Windows.Forms.Label searchOutputLabel;
+        private System.Windows.Forms.TextBox randomOutputTextBox;
+    }
+}
+
---
+++ GUI_Array_Search/GUI_Array_Search/GUI_Array_Search/mainForm.cs
@@ -0,0 +1,81 @@
+﻿// Program Name:    T05 P03 GUI Array Search
+// Author:          Karim Khan
+//
+// Date:            07-April-2020
+//
+// Description:     Mini-Project Assignment ‐ Array Practice Programs
+//
+//                  Create a GUI app that generates 10 random numbers, stores them in an array, 
+//                  and allows the user to search for a particular value, displaying the index 
+//                  if found.If the value is not in the list of numbers then that should be reported 
+//                  to the user with an appropriate message.
+//                  The random numbers should all be two digit numbers; i.e.between 10 and 99.
+//
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GUI_Array_Search
+{
+    public partial class mainForm : Form
+    {
+        // define list dimensions, minimum and maximum
+        
+        const int LIST_SIZE = 10;
+        const int RAND_MIN   = 10;
+        const int RAND_MAX   = 99;
+
+        private List <int> myRandomNumbers = new List <int>(LIST_SIZE); // model used to generate random numbers
+        public mainForm()
+        {
+            InitializeComponent();
+        }
+        // use Form Load event to trigger the generation of the list values and the subsequent display to the user
+        
+        private void mainForm_Load(object sender, EventArgs e)
+        {
+            System.Random random = new System.Random(); // activate random number generator
+
+            // drop into the for loop to pull out the generated random numbers and add them to the list, 
+            // accounting for the defined bounds
+            
+            for (int i = 0; i < LIST_SIZE; i++)
+            {
+                myRandomNumbers.Add(random.Next(RAND_MIN, RAND_MAX));
+            }
+            // use foreach loop to pull out the generated numbers from the collection and view them
+            
+            foreach (int number in myRandomNumbers)
+            {
+                randomOutputTextBox.Text += $"{number,4}"; // view generated random numbers
+            }
+        }
+
+        private void searchButton_Click(object sender, EventArgs e)
+        {
+            // index value is used to pin-point index in the list
+            
+            int index = myRandomNumbers.IndexOf(int.Parse(searchTextBox.Text.Trim()));
+            
+            // if-else statement used to assess entered user value in the search box
+
+            if (index == -1) // if index returns a value of -1, output message that value is not found
+            {
+                searchOutputLabel.Visible = true;
+                searchOutputLabel.Text = $"The value {searchTextBox.Text.Trim()} was NOT found.";
+            }
+
+            else
+            {
+                searchOutputLabel.Visible = true;
+                searchOutputLabel.Text = $"The value {searchTextBox.Text.Trim()} was found at index {index}.";
+            }
+        }
+    }
+}
